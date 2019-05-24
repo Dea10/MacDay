4bebe1f4030c44793a5bb57a5d04e5e80525a8f7
DEA <d111092.de@gmail.com>
2019-05-23 23:26:31 -0500

    creacion de menu

---
+++ src/GeneradorNum.java
@@ -0,0 +1,50 @@
+import java.util.Random;
+import static java.lang.Math.pow;
+
+public class GeneradorNum extends Thread{
+    public int passwordNum;
+    public int crackNum;
+    public int tam;
+    public int pass;
+    public int inicio;
+    public int fin;
+    public int i, j, aux;
+    Random rand = new Random();
+
+    public GeneradorNum (int crackNum, int tam, int inicio, int fin){
+        this.tam = tam;
+        this.crackNum = crackNum;
+        this.inicio = inicio;
+        this.fin = fin;
+    }
+
+    public void run(){
+        long startTime = System.nanoTime();
+
+        while (passwordNum != crackNum){
+            passwordNum = generaPassNum();
+        }
+
+        long endTime = System.nanoTime();
+
+        System.out.println("\n\nContraseña descifrada: " + passwordNum);
+        System.out.println("Tiempo: " + ((endTime-startTime)/1e6)/1000 + " seg.\n");
+    }
+
+    public int generaPassNum(){
+        passwordNum = 0;
+
+        for (i=0; i<tam; i++){
+            //passwordNum = rand.nextInt((int)pow(10, tam)-1);
+            passwordNum = rand.nextInt(fin-inicio+1)+inicio;
+        }
+
+        return passwordNum;
+    }
+
+    public void limpiarPantalla(){
+        for (int i=0; i<50; i++){
+            System.out.println();
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,88 @@
+import java.util.Scanner;
+
+import static java.lang.Math.pow;
+
 public class Main {
     public static void main(String[] args){
 
-        String p1 = "passdea";
+        int inputPassword; // int max 999,999,999
+        int pMax;
+        int respuesta;
+        Scanner sc = new Scanner(System.in);
 
-        GeneradorSimpleThread g1 = new GeneradorSimpleThread(p1, p1.length());
+        imprimirBienvenida();
+
+        //do {
+            respuesta = imprimirMenu();
+
+            switch (respuesta){
+                case 1:
+                    limpiarPantalla();
+                    System.out.print("Introduce un password numérico a descifrar: ");
+                    inputPassword = sc.nextInt(); //agregar try catch y validación para int max
+                    pMax = (int)pow(10, Integer.toString(inputPassword).length())-1; // int pMax 999,999,999
+                    ejecutarAlgoritmo(inputPassword, pMax);
+                    break;
+                case 2:
+                    limpiarPantalla();
+                    System.out.print("Introduce un password numérico a descifrar: ");
+                    inputPassword = sc.nextInt(); //agregar try catch y validación para int max
+                    pMax = (int)pow(10, Integer.toString(inputPassword).length())-1; // int pMax 999,999,999
+                    ejecutarAlgoritmoParalelo(inputPassword, pMax);
+                    break;
+                default:
+                    System.out.println("\nPor favor, fíjate en el dato que introduces :)");
+            }
+        //}while(respuesta != 666);
+    }
+
+    public static void imprimirBienvenida(){
+        System.out.println("\n\n:: Programación Paralela aplicada a Seguridad Computacional ::");
+
+        System.out.println("\n\t\t\t\t\t\tBienvenido!");
+        System.out.println("\nEl siguiente programa tiene como propósito mostrar los beneficios de la programación paralela, para ello" +
+                " se desarrolló una pequeña aplicación orientada a la Seguridad Computacional.");
+
+        System.out.println("\n\n:: Alumnos ::");
+        System.out.println("\n\tEspinosa Arias Daniel.");
+        System.out.println("\tGonzález Sainz Sergio Abraham.");
+
+        System.out.println("\n\n* - * - * - * - * - * - * - * - * - * - * - *");
+    }
+
+    public static int imprimirMenu(){
+        System.out.println("\n\n:: MENÚ ::");
+        System.out.println("\n\t1. Ejecutar algoritmo.");
+        System.out.println("\t2. Ejecutar algoritmo en paralelo.");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("\n\t\t\t\t-> INTRODUCIR OPCIÓN A EJECUTAR: ");
+        int respuesta = sc.nextInt();
+
+        return respuesta;
+    }
+
+    public static void limpiarPantalla(){
+        for (int i=0; i<50; i++){
+            System.out.println();
+        }
+    }
+
+    public static void ejecutarAlgoritmo(int p, int pMax){
+        GeneradorNum g0 = new GeneradorNum(p, Integer.toString(p).length(), 0, pMax);
+        g0.start();
+    }
+
+    public static void ejecutarAlgoritmoParalelo(int p, int pMax){
+        GeneradorNum g1 = new GeneradorNum(p, Integer.toString(p).length(), 0, pMax/4);
+        GeneradorNum g2 = new GeneradorNum(p, Integer.toString(p).length(), pMax/4, pMax/2);
+        GeneradorNum g3 = new GeneradorNum(p, Integer.toString(p).length(),pMax/2, 3*(pMax/4));
+        GeneradorNum g4 = new GeneradorNum(p, Integer.toString(p).length(),3*(pMax/4), pMax);
 
         g1.start();
+        g2.start();
+        g3.start();
+        g4.start();
     }
 }
